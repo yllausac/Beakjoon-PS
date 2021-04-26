9326335db36e33826f384ed1591b72ae17b280ee
yllausac <dadanana0805@gmail.com>
2021-04-26 22:32:14 +0900

    BOJ

---
+++ 15654번 N과 M (5).py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+number = list(map(int, input().split()))
+number.sort()
+array = []
+check = [False]*n
+
+def dfs(x):
+    if x == m:
+        print(*array)
+        return
+
+    for i in range(n):
+        if check[i] == False:
+            array.append(number[i])
+            check[i] = True
+            dfs(x+1)
+            array.pop()
+            check[i] = False
+
+dfs(0)
