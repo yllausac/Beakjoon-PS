1dbfa8698d43cc44941170bf694cc079f9ad9890
yllausac <dadanana0805@gmail.com>
2021-05-02 19:44:44 +0900

    BOJ

--- 16198번 에너지 모으기.py
+++ 16198번 에너지 모으기.py
@@ -1,9 +1,15 @@
 n = int(input())
 w = list(map(int, input().split()))
-energe = 0
-
-for i in range(1, n):
-    energe = max(w[i] + w[i+1], energe)
 
 
-print(energe)
+def E(w):
+    if len(w) == 3:
+        return w[0]*w[2]
+    energe = 0
+    for i in range(1, len(w)-1):
+        r = w[i-1] * w[i+1] + E(w[:i] + w[i+1:])
+        energe = max(energe, r)
+    return energe
+
+
+print(E(w))
