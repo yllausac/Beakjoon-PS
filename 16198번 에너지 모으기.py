26b0db74c1ce061696f7cff8772ac0d779949865
yllausac <dadanana0805@gmail.com>
2021-05-02 19:50:30 +0900

    BOJ

--- 16198번 에너지 모으기.py
+++ 16198번 에너지 모으기.py
@@ -4,7 +4,7 @@ w = list(map(int, input().split()))
 
 def E(w):
     if len(w) == 3:
-        return w[0]*w[2]
+        return w[0] * w[2]
     energe = 0
     for i in range(1, len(w)-1):
         r = w[i-1] * w[i+1] + E(w[:i] + w[i+1:])
