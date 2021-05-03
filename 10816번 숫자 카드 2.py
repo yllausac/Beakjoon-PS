7298e7eeb8dadeec75203177537470922ad8ccca
yllausac <dadanana0805@gmail.com>
2021-05-03 18:30:17 +0900

    BOJ

--- 10816번 숫자 카드 2.py
+++ 10816번 숫자 카드 2.py
@@ -1,31 +1,14 @@
-n = int(input())
-card = list(map(int, input().split()))
-m = int(input())
-check = list(map(int, input().split()))
+from sys import stdin
 
-card.sort()
-num = dict()
-for i in card:
-    num.setdefault(i)
-    num[i] = card.count(i)
-
-
-def binary_search(n, card, start, end):
-    if start > end:
-        return 0
-    m = (start+end)//2
-    if n == card[m]:
-        return card[start:end+1].count(n)
-    elif n < card[m]:
-        return binary_search(n, card, start, m-1)
+_ = stdin.readline()
+N = map(int, stdin.readline().split())
+_ = stdin.readline()
+M = map(int, stdin.readline().split())
+hashmap = {}
+for n in N:
+    if n in hashmap:
+        hashmap[n] += 1
     else:
-        return binary_search(n, card, m+1, end)
+        hashmap[n] = 1
 
-n_dic = {}
-for n in card:
-    start = 0
-    end = len(card) - 1
-    if n not in n_dic:
-        n_dic[n] = binary_search(n, card, start, end)
-
-print(' '.join(str(n_dic[x]) if x in n_dic else '0' for x in check))
+print(' '.join(str(hashmap[m]) if m in hashmap else '0' for m in M))
