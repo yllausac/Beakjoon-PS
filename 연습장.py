f31464c8aca268756ada2eb8864237a4a2bccf95
yllausac <dadanana0805@gmail.com>
2021-06-27 18:10:42 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,17 +1,42 @@
-import random
+from copy import deepcopy
 
-TRIALS = 100000
-same_birthdays = 0
+n = int(input())
+s = []
+for _ in range(n):
+    s.append(list(map(int, input().split())))
 
-for _ in range(TRIALS):
-    birthdays = []
-    for i in range(23):
-        birthday = random.randint(1, 365)
-        if birthday in birthdays:
-            same_birthdays += 1
-            break
-        birthdays.append(birthday)
 
-print(f'{same_birthdays / TRIALS*100}%')
+def dif(start):
+    global result
+    link = []
+    for i in range(n):
+        if i in start:
+            continue
+        link.append(i)
+    start_n = 0
+    link_n = 0
+    for i in range(n//2 - 1):
+        for j in range(i+1, n // 2):
+            a, b = start[i], start[j]
+            c, d = link[i], link[j]
+            start_n += s[a][b] + s[b][a]
+            link_n += s[c][d] + s[d][c]
+    result = min(result, abs(start_n - link_n))
 
+
+def dfs(temp):
+    t = deepcopy(temp)
+    if len(t) == n//2:
+        dif(t)
+        return
+    for i in range(t[-1] + 1, n):
+        t.append(i)
+        dfs(t)
+        t.pop()
+
+
+result = 1000000000
+temp = [0]
+dfs(temp)
+print(result)
 
