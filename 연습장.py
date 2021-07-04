ae66ce5b08cf2e264a78c5390dd4d413d4762aab
yllausac <dadanana0805@gmail.com>
2021-07-04 22:06:57 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,41 +1,9 @@
-from copy import deepcopy
-
-n = int(input())
-s = []
-for _ in range(n):
-    s.append(list(map(int, input().split())))
-
-
-def dif(start):
-    global result
-    link = []
-    for i in range(n):
-        if i in start:
-            continue
-        link.append(i)
-    start_n = 0
-    link_n = 0
-    for i in range(n//2 - 1):
-        for j in range(i+1, n // 2):
-            a, b = start[i], start[j]
-            c, d = link[i], link[j]
-            start_n += s[a][b] + s[b][a]
-            link_n += s[c][d] + s[d][c]
-    result = min(result, abs(start_n - link_n))
-
-
-def dfs(temp):
-    t = deepcopy(temp)
-    if len(t) == n//2:
-        dif(t)
-        return
-    for i in range(t[-1] + 1, n):
-        t.append(i)
-        dfs(t)
-        t.pop()
-
-
-result = 1000000000
-temp = [0]
-dfs(temp)
-print(result)
+n = list(input())
+n.sort(reverse=True)
+sum = 0
+for i in n:
+    sum += int(i)
+if sum % 3 != 0 or "0" not in n:
+    print(-1)
+else:
+    print(''.join(n))
