c051bcf71cf5db83e3953768f6f29d7835241eb3
yllausac <dadanana0805@gmail.com>
2021-07-07 19:45:58 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,16 +1,41 @@
+from copy import deepcopy
+
 n = int(input())
-a = list(map(int, input().split()))
-b, c = map(int, input().split())
-count = 0
+s = []
+for _ in range(n):
+    s.append(list(map(int, input().split())))
 
-for i in a:
-    if b > i:
-        count += 1
-    else:
-        ans = b
-        count += 1
-        while ans < i:
-            ans += c
-            count += 1
 
-print(count)
+def dif(start):
+    global result
+    link = []
+    for i in range(n):
+        if i in start:
+            continue
+        link.append(i)
+    start_n = 0
+    link_n = 0
+    for i in range(n//2 - 1):
+        for j in range(i+1, n // 2):
+            a, b = start[i], start[j]
+            c, d = link[i], link[j]
+            start_n += s[a][b] + s[b][a]
+            link_n += s[c][d] + s[d][c]
+    result = min(result, abs(start_n - link_n))
+
+
+def dfs(temp):
+    t = deepcopy(temp)
+    if len(t) == n//2:
+        dif(t)
+        return
+    for i in range(t[-1] + 1, n):
+        t.append(i)
+        dfs(t)
+        t.pop()
+
+
+result = 1000000000
+temp = [0]
+dfs(temp)
+print(result)
