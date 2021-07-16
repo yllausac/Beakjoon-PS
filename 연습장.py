c8b9014b1195c11fef8b302e1569d9a6da914b8d
yllausac <dadanana0805@gmail.com>
2021-07-16 13:11:08 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,16 +1,33 @@
 n = int(input())
-a = list(map(int, input().split()))
-b, c = map(int, input().split())
-count = 0
-
-for i in a:
-    if b > i:
-        count += 1
+posi = []
+nega = []
+result = 0
+for _ in range(n):
+    a = int(input())
+    if a > 1:
+        posi.append(a)
+    elif a == 1:
+        result += 1
     else:
-        ans = b
-        count += 1
-        while ans < i:
-            ans += c
-            count += 1
+        nega.append(a)
 
-print(count)
+posi.sort(reverse=True)
+nega.sort()
+
+if len(posi) % 2 == 0:
+    for i in range(0, len(posi), 2):
+        result += posi[i] * posi[i+1]
+else:
+    for i in range(0, len(posi)-1, 2):
+        result += posi[i] * posi[i+1]
+    result += posi[len(posi)-1]
+
+if len(nega) % 2 == 0:
+    for i in range(0, len(nega), 2):
+        result += nega[i] * nega[i+1]
+else:
+    for i in range(0, len(nega)-1, 2):
+        result += nega[i] * nega[i+1]
+    result += nega[len(nega)-1]
+
+print(result)
