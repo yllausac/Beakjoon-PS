77873e91022b519bd12055c7a2ab5f3db62816ce
yllausac <dadanana0805@gmail.com>
2021-07-20 16:51:23 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,89 +1,38 @@
-import sys, copy
-
-input = sys.stdin.readline
-
-n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
-ans = 0
+import sys
+from collections import deque
 
 
-def move(dir):
-    if dir == 0:  # 위로 이동
-        for j in range(n):
-            idx = 0
-            for i in range(1, n):
-                if board[i][j]:  # 숫자가 있는 칸이면
-                    temp = board[i][j]  # temp에 숫자 저장
-                    board[i][j] = 0  # 0으로 바꿔줌
-                    if board[idx][j] == 0:
-                        board[idx][j] = temp
-                    elif board[idx][j] == temp:
-                        board[idx][j] = temp * 2
-                        idx += 1
-                    else:
-                        idx += 1
-                        board[idx][j] = temp
-    elif dir == 1:  # 아래로 이동
-        for j in range(n):
-            idx = n - 1
-            for i in range(n - 2, -1, -1):
-                if board[i][j]:
-                    temp = board[i][j]
-                    board[i][j] = 0
-                    if board[idx][j] == 0:
-                        board[idx][j] = temp
-                    elif board[idx][j] == temp:
-                        board[idx][j] = temp * 2
-                        idx -= 1
-                    else:
-                        idx -= 1
-                        board[idx][j] = temp
-
-    elif dir == 2:  # 좌로 이동
-        for i in range(n):
-            idx = 0
-            for j in range(1, n):
-                if board[i][j]:
-                    temp = board[i][j]
-                    board[i][j] = 0
-                    if board[i][idx] == 0:
-                        board[i][idx] = temp
-                    elif board[i][idx] == temp:
-                        board[i][idx] = temp * 2
-                        idx += 1
-                    else:
-                        idx += 1
-                        board[i][idx] = temp
-    else:  # 우로 이동
-        for i in range(n):
-            idx = n - 1
-            for j in range(n-2, -1, -1):
-                if board[i][j]:
-                    temp = board[i][j]
-                    board[i][j] = 0
-                    if board[i][idx] == 0:
-                        board[i][idx] = temp
-                    elif board[i][idx] == temp:
-                        board[i][idx] = temp * 2
-                        idx -= 1
-                    else:
-                        idx -= 1
-                        board[i][idx] = temp
-
-
-def dfs(cnt):
-    global board, ans
-    if cnt == 5:
-        for i in range(n):
-            for j in range(n):
-                ans = max(ans, board[i][j])
+def check_right(start, dirs):
+    if start > 4 or gears[start-1][2] == gears[start][6]:
         return
-    temp_a = copy.deepcopy(board)
-    for i in range(4):
-        move(i)
-        dfs(cnt + 1)
-        board = copy.deepcopy(temp_a)
+
+    if gears[start-1][2] != gears[start][6]:
+        check_right(start + 1, -dirs)
+        gears[start].rotate(dirs)
 
 
-dfs(0)
-print(ans)
+def check_left(start, dirs):
+    if start < 1 or gears[start][2] == gears[start+1][6]:
+        return
+
+    if gears[start+1][6] != gears[start][2]:
+        check_left(start - 1, -dirs)
+        gears[start].rotate(dirs)
+
+
+gears = {}
+for i in range(1, 5):
+    gears[i] = deque(list(map(int, list(sys.stdin.readline().replace("\n", "")))))
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    num, dirs = map(int, sys.stdin.readline().split())
+
+    check_right(num+1, -dirs)
+    check_left(num-1, -dirs)
+    gears[num].rotate(dirs)
+
+result = 0
+for i in range(4):
+    result += (2**i) * gears[i+1][0]
+print(result)
