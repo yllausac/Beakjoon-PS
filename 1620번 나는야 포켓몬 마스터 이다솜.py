3553a373ec411c2fbafe70649d4a78b215ee468f
yllausac <dadanana0805@gmail.com>
2021-07-21 21:55:58 +0900

    BOJ

---
+++ 1620번 나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+arr_int = {}
+arr_name = {}
+for i in range(n):
+    poketmon = input()
+    arr_int[i+1] = poketmon
+    arr_name[poketmon] = i+1
+
+for _ in range(m):
+    order = input()
+    if order.isdigit():
+        print(arr_int[int(order)])
+    else:
+        print(arr_name[order])
--- 연습장.py
+++ 연습장.py
@@ -1,38 +1,16 @@
-import sys
-from collections import deque
+import random
 
+TRIALS = 100000
+same_birthdays = 0
 
-def check_right(start, dirs):
-    if start > 4 or gears[start-1][2] == gears[start][6]:
-        return
+for _ in range(TRIALS):
+    birthdays = []
+    for i in range(100):
+        birthday = random.randint(1, 365)
+        if birthday in birthdays:
+            same_birthdays += 1
+            break
+        else:
+            birthdays.append(birthday)
 
-    if gears[start-1][2] != gears[start][6]:
-        check_right(start + 1, -dirs)
-        gears[start].rotate(dirs)
-
-
-def check_left(start, dirs):
-    if start < 1 or gears[start][2] == gears[start+1][6]:
-        return
-
-    if gears[start+1][6] != gears[start][2]:
-        check_left(start - 1, -dirs)
-        gears[start].rotate(dirs)
-
-
-gears = {}
-for i in range(1, 5):
-    gears[i] = deque(list(map(int, list(sys.stdin.readline().replace("\n", "")))))
-n = int(sys.stdin.readline())
-
-for _ in range(n):
-    num, dirs = map(int, sys.stdin.readline().split())
-
-    check_right(num+1, -dirs)
-    check_left(num-1, -dirs)
-    gears[num].rotate(dirs)
-
-result = 0
-for i in range(4):
-    result += (2**i) * gears[i+1][0]
-print(result)
+print(f'{same_birthdays / TRIALS * 100}%')
