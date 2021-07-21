be40ed7bab0f8dd50948aa09307984c6417dabb4
yllausac <dadanana0805@gmail.com>
2021-07-21 22:00:39 +0900

    BOJ

--- 1620번 나는야 포켓몬 마스터 이다솜.py
+++ 1620번 나는야 포켓몬 마스터 이다솜.py
@@ -5,12 +5,12 @@ n, m = map(int, input().split())
 arr_int = {}
 arr_name = {}
 for i in range(n):
-    poketmon = input()
+    poketmon = input().strip()
     arr_int[i+1] = poketmon
     arr_name[poketmon] = i+1
 
 for _ in range(m):
-    order = input()
+    order = input().strip()
     if order.isdigit():
         print(arr_int[int(order)])
     else:
