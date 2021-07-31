8220db9c69fb9ab86a5bf1d84c5c0b5ebaf4ef2c
yllausac <dadanana0805@gmail.com>
2021-07-31 16:31:27 +0900

    BOJ

--- 2533번 사회망 서비스(SNS).py
+++ 2533번 사회망 서비스(SNS).py
@@ -18,7 +18,9 @@ def adapter(node):
     for i in graph[node]:
         if visit[i] == 0:
             adapter(i)
+            # i 노드가 얼리어답터일 때의 서브트리에서 얼리어답터의 최소값
             dp[node][0] += min(dp[i][0], dp[i][1])
+            # i 노드가 얼리어답터가 아닐 때의 서브트리에서 얼리어답터의 최소값
             dp[node][1] += dp[i][0]
 
 
