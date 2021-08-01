473d44b1078f933344ebfc9c76cd7b8e32ced8f3
yllausac <dadanana0805@gmail.com>
2021-08-01 18:42:50 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,37 +1,13 @@
-import sys
-import heapq
+class TreeNode:
+    def __init__(self, val=0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
-input = sys.stdin.readline
-dx, dy = [1, -1, 0, 0], [0, 0, -1, 1]
-cnt = 1
-
-
-def Dijkstra():
-    dp = [[1e9] * n for _ in range(n)]
-    dp[0][0] = graph[0][0]
-    visit = [[0] * n for _ in range(n)]
-    heap = []
-    heapq.heappush(heap, [graph[0][0], 0, 0])
-    while heap:
-        c, a, b = heapq.heappop(heap)
-        for k in range(4):
-            x = a + dx[k]
-            y = b + dy[k]
-            if 0 <= x < n and 0 <= y < n and visit[x][y] == 0:
-                dp[x][y] = min(dp[x][y], dp[a][b] + graph[x][y])
-                heapq.heappush(heap, [dp[x][y], x, y])
-                visit[x][y] = 1
-    return dp[n-1][n-1]
-
-
-while True:
-    n = int(input())
-    if n == 0:
-        break
-    graph = []
-    for i in range(n):
-        graph.append(list(map(int, input().split())))
-    result = Dijkstra()
-    print("Problem %d: %d" % (cnt, result))
-    cnt += 1
+class Solution:
+    def mergeTrees(selfself, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if t1 and t2:
+            node = TreeNode(t1.val + t2.val)
+            node.left = self.mergeTrees(t1.left, t2.left)
+            
