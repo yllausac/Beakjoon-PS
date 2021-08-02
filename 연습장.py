d74f559b4d1dd1b6bc0bea3fe719f5076ca8f5c7
yllausac <dadanana0805@gmail.com>
2021-08-02 17:21:10 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,13 +1,25 @@
-class TreeNode:
-    def __init__(self, val=0, left = None, right = None):
-        self.val = val
-        self.left = left
-        self.right = right
+n = 10
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+tree = [0] * (n*4)
 
 
-class Solution:
-    def mergeTrees(selfself, t1: TreeNode, t2: TreeNode) -> TreeNode:
-        if t1 and t2:
-            node = TreeNode(t1.val + t2.val)
-            node.left = self.mergeTrees(t1.left, t2.left)
-            
+def init(start, end, index):
+    if start == end:
+        tree[index] = array[start]
+        return tree[index]
+    mid = (start + end) // 2
+    tree[index] = init(start, mid, index*2) + init(mid+1, end, index*2+1)
+
+
+def query(start, end, index, qleft, qright):
+    if qleft > end or qright < start:
+        return 0
+    if qleft <= start and end <= qright:
+        return tree[index]
+    mid = (start+end)//2
+    return query(start, mid, index*2, qleft, qright) + query(mid+1, end, index*2+1, qleft,qright)
+
+
+init(1, n, 1)
+s, e = map(int, input().split())
+print(query(1, n, 1, s, e))
