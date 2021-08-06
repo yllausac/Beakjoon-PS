7ce00fd69c005ca0d3d87c57ec64e329107ae98e
yllausac <dadanana0805@gmail.com>
2021-08-06 15:18:15 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,30 +1,28 @@
 import sys
 
+sys.setrecursionlimit(10 ** 9)
 input = sys.stdin.readline
 n = int(input())
-budgets = list(map(int, input().split()))
-budgets.sort()
-m = int(input())
+graph = [[]for _ in range(n+1)]
+for _ in range(n-1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+visit = [0 for _ in range(n+1)]
+dp = [[0, 0] for _ in range(n+1)]
 
 
-def binary_search(budgets):
-    left = 0
-    right = budgets[-1]
-
-    while left <= right:
-        mid = (left + right) // 2
-        num = 0
-        for budget in budgets:
-            if budget >= mid:
-                num += mid
-            else:
-                num += budget
-        if num <= m:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return right
+def adapter(node):
+    visit[node] = 1
+    dp[node][0] = 1
+    for i in graph[node]:
+        if visit[i] == 0:
+            adapter(i)
+            # i 노드가 얼리어답터일 때의 서브트리에서 얼리어답터의 최소값
+            dp[node][0] += min(dp[i][0], dp[i][1])
+            # i 노드가 얼리어답터가 아닐 때의 서브트리에서 얼리어답터의 최소값
+            dp[node][1] += dp[i][0]
 
 
-print(binary_search(budgets))
+adapter(1)
+print(min(dp[1][0], dp[1][1]))
