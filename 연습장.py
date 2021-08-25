6ba2d65bdeb55da8e9567c4e98bbeb7ed9f60c7b
yllausac <dadanana0805@gmail.com>
2021-08-25 17:56:59 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,50 +1,43 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
-n, m = map(int, input().split())
-before = [list(input().strip().split()) for _ in range(n)]
-after = [list(input().strip().split()) for _ in range(n)]
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+n = int(input())
+m = int(input())
+parent = [0] * (n+1)
+for i in range(1, n+1):
+    parent[i] = i
 
 
-def solv():
-    for x in range(n):
-        for y in range(m):
-            if before[x][y] != after[x][y]:
-                bfs(x, y)
-                return check()
-    return 'YES'
+def find(a):  # a 노드의 부모노드 찾기
+    if a == parent[a]:
+        return a
+    p = find(parent[a])
+    parent[a] = p
+    return parent[a]
 
 
-def bfs(x, y):
-    global before
-    visited = [[False] * m for _ in range(n)]
-    queue = deque([(x, y)])
-
-    visited[x][y] = True
-    target = before[x][y]
-    num = after[x][y]
-    while queue:
-        cx, cy = queue.pop()
-        before[cx][cy] = num
-
-        for k in range(4):
-            nx = cx + dx[k]
-            ny = cy + dy[k]
-            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == False:
-                if before[nx][ny] == target:
-                    visited[nx][ny] = True
-                    queue.appendleft((nx, ny))
+def union(a, b):  # a집합과 b집합 합치기
+    a = find(a)
+    b = find(b)
+    if a == b:
+        return  # 동일한 집합이면 연결시에 순환이 발생
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
 
-def check():
-    for x in range(n):
-        for y in range(m):
-            if before[x][y] != after[x][y]:
-                return 'NO'
-    return 'YES'
+for y in range(1, n+1):  # y번째 도시
+    graph = list(map(int, input().split()))
+    for x in range(1, len(graph)+1):
+        if graph[x-1] == 1:
+            union(y, x)
 
+plan = list(map(int, input().split()))
+result = set([find(city) for city in plan])
+if len(result) != 1:
+    print('NO')
+else:
+    print('YES')
 
-print(solv())
 
