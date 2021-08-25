c25e1e655cd61c60872aef1d24d2e28f590e1ac2
yllausac <dadanana0805@gmail.com>
2021-08-25 17:54:59 +0900

    BOJ

--- 1976번 여행 가자.py
+++ 1976번 여행 가자.py
@@ -8,7 +8,7 @@ for i in range(1, n+1):
     parent[i] = i
 
 
-def find(a):
+def find(a):  # a 노드의 부모노드 찾기
     if a == parent[a]:
         return a
     p = find(parent[a])
@@ -16,18 +16,18 @@ def find(a):
     return parent[a]
 
 
-def union(a, b):
+def union(a, b):  # a집합과 b집합 합치기
     a = find(a)
     b = find(b)
     if a == b:
-        return
+        return  # 동일한 집합이면 연결시에 순환이 발생
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
 
 
-for y in range(1, n+1):
+for y in range(1, n+1):  # y번째 도시
     graph = list(map(int, input().split()))
     for x in range(1, len(graph)+1):
         if graph[x-1] == 1:
