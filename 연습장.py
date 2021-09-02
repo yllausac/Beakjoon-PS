6a4a09f293c15b6f3479336f1a9815d31b83c40c
yllausac <dadanana0805@gmail.com>
2021-09-02 18:36:20 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,35 +1,29 @@
 import sys
+import heapq
 
 input = sys.stdin.readline
-n = int(input())
-posi, nega = [], []
-result = 0
+n, k = map(int, input().split())
+wei, val = [], []
+gem = []
 for _ in range(n):
-    num = int(input())
-    if num > 1:
-        posi.append(num)
-    elif num == 1:
-        result += 1
-    else:
-        nega.append(num)
+    m, v = map(int, input().split())
+    heapq.heappush(gem, [m, v])
+bag = []
+for _ in range(k):
+    capacity = int(input())
+    heapq.heappush(bag, capacity)
 
-posi.sort(reverse=True)
-nega.sort()
+total_value = 0
+capable_gem = []
 
-if len(posi) % 2 == 0:
-    for i in range(0, len(posi), 2):
-        result += posi[i] * posi[i+1]
-else:
-    for i in range(0, len(posi)-1, 2):
-        result += posi[i] * posi[i+1]
-    result += posi[len(posi)-1]
+for _ in range(k):
+    capacity = heapq.heappop(bag)
+    while gem and capacity >= gem[0][0]:
+        [m, v] = heapq.heappop(gem)
+        heapq.heappush(capable_gem, -v)
+    if capable_gem:
+        total_value -= heapq.heappop(capable_gem)
+    elif not gem:
+        break
 
-if len(nega) % 2 == 0:
-    for i in range(0, len(nega), 2):
-        result += nega[i] * nega[i+1]
-else:
-    for i in range(0, len(nega)-1, 2):
-        result += nega[i] * nega[i+1]
-    result += nega[len(nega)-1]
-
-print(result)
+print(total_value)
