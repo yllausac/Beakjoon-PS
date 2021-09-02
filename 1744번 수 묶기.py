605cc2fbcb785f492b246097429a1a5a8eaa3239
yllausac <dadanana0805@gmail.com>
2021-09-02 11:33:51 +0900

    BOJ

--- 1744번 수 묶기.py
+++ 1744번 수 묶기.py
@@ -1,33 +1,36 @@
+import sys
+
+input = sys.stdin.readline
 n = int(input())
-posi = []
-nega = []
+posi, nega = [], []
 result = 0
 for _ in range(n):
-    a = int(input())
-    if a > 1:
-        posi.append(a)
-    elif a == 1:
+    num = int(input())
+    if num > 1:
+        posi.append(num)
+    elif num == 1:
         result += 1
     else:
-        nega.append(a)
+        nega.append(num)
 
 posi.sort(reverse=True)
 nega.sort()
 
 if len(posi) % 2 == 0:
     for i in range(0, len(posi), 2):
-        result += posi[i] * posi[i+1]
+        result += posi[i] * posi[i + 1]
 else:
-    for i in range(0, len(posi)-1, 2):
-        result += posi[i] * posi[i+1]
-    result += posi[len(posi)-1]
+    for i in range(0, len(posi) - 1, 2):
+        result += posi[i] * posi[i + 1]
+    result += posi[len(posi) - 1]
 
 if len(nega) % 2 == 0:
     for i in range(0, len(nega), 2):
-        result += nega[i] * nega[i+1]
+        result += nega[i] * nega[i + 1]
 else:
-    for i in range(0, len(nega)-1, 2):
-        result += nega[i] * nega[i+1]
-    result += nega[len(nega)-1]
+    for i in range(0, len(nega) - 1, 2):
+        result += nega[i] * nega[i + 1]
+    result += nega[len(nega) - 1]
 
 print(result)
+
