d8b62492b965c7479a4e290b345c54c0d5b73db5
yllausac <dadanana0805@gmail.com>
2021-09-09 22:53:44 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,15 +1,32 @@
 import sys
 
 input = sys.stdin.readline
-n = int(input())
-pen = list(map(int, input().split()))
-pen.sort()
-target = 1
-for x in pen:
-    if target < x:
-        break
-    target += x
+R, C = map(int, input().split())
+graph = []
+for _ in range(R):
+    graph.append(list(input().rstrip()))
+dx = [-1, 0, 1]
+visit = [[False]*C for _ in range(R)]
+ans = 0
 
-print(target)
+
+def solve(i, j):
+    if j == C-1:
+        return True
+
+    for d in dx:
+        if 0 <= i + d < R and graph[i+d][j+1] == '.' and not visit[i+d][j+1]:
+            visit[i+d][j+1] = True
+            if solve(i+d, j+1):
+                return True
+    return False
+
+
+for i in range(R):
+    if graph[i][0] == '.':
+        if solve(i, 0):
+            ans += 1
+
+print(ans)
 
 
