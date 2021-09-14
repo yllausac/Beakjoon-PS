bd518c7ab2de58d8f61151c4eb652ac6d1e6f972
yllausac <dadanana0805@gmail.com>
2021-09-14 20:23:06 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -2,37 +2,16 @@ import sys
 
 input = sys.stdin.readline
 n = int(input())
-posi, nega = [], []
-result = 0
-for _ in range(n):
-    num = int(input())
-    if num > 1:
-        posi.append(num)
-    elif num == 1:
-        result += 1
-    else:
-        nega.append(num)
+pen = list(map(int, input().split()))
+pen.sort()
+target = 1
+for x in pen:
+    if target < x:
+        break
+    target += x
 
-posi.sort(reverse=True)
-nega.sort()
+print(target)
 
-if len(posi) % 2 == 0:
-    for i in range(0, len(posi), 2):
-        result += posi[i] * posi[i + 1]
-else:
-    for i in range(0, len(posi) - 1, 2):
-        result += posi[i] * posi[i + 1]
-    result += posi[len(posi) - 1]
-
-if len(nega) % 2 == 0:
-    for i in range(0, len(nega), 2):
-        result += nega[i] * nega[i + 1]
-else:
-    for i in range(0, len(nega) - 1, 2):
-        result += nega[i] * nega[i + 1]
-    result += nega[len(nega) - 1]
-
-print(result)
 
 
 
