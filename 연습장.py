cd52fcc845cc438cf5f30fe2ee788917478dadb5
yllausac <dadanana0805@gmail.com>
2021-09-18 16:07:22 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,32 +1,30 @@
-import heapq
+import sys
 
-n, k = map(int, input().split())
-gem = []
-for _ in range(n):
-    m, v = map(int, input().split())
-    heapq.heappush(gem, [m, v])  # gem에다가 [m, v]를 넣음
-
-bag = []
-for _ in range(k):
-    capacity = int(input())
-    heapq.heappush(bag, capacity)  # bag에다 capacity를 넣음
-
-total_value = 0
-capable_gem = []
-
-for _ in range(k):
-    capacity = heapq.heappop(bag)  # bag의 최솟값을 heappop
-    while gem and capacity >= gem[0][0]:  # 현재 bag의 capacity보다 이하인 모든 보석
-        [m, v] = heapq.heappop(gem)  # 최소 무게부터 꺼낸다
-        heapq.heappush(capable_gem, -v)  # 무게를 제외한 값만 heappush하여 넣어준다
-    if capable_gem:  # gem 최소보다는 작지만 넣을 수 있는 보석은 있는 경우
-        total_value -= heapq.heappop(capable_gem)
-    elif not gem:  # 남은 보석이 없는 경우
-        break
-
-print(total_value)
+input = sys.stdin.readline
+r, c = map(int, input().split())
+graph = []
+for _ in range(r):
+    graph.append(list(input()))
+dx = [-1, 0, 1]
+visit = [[False]*c for _ in range(r)]
+ans = 0
 
 
+def solve(i, j):
+    if j == c - 1:
+        return True
+
+    for d in dx:
+        if 0 <= i + d < r and graph[i+d][j+1] == '.' and not visit[i+d][j+1]:
+            visit[i+d][j+1] = True
+            if solve(i+d, j+1):
+                return True
+    return False
 
 
+for i in range(r):
+    if graph[i][0] == '.':
+        if solve(i, 0):
+            ans += 1
 
+print(ans)
