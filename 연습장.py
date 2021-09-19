20611b799c6af6aa4e09a1cb59ccd72876c8d29c
yllausac <dadanana0805@gmail.com>
2021-09-19 16:48:22 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,30 +1,35 @@
 import sys
 
 input = sys.stdin.readline
-r, c = map(int, input().split())
-graph = []
-for _ in range(r):
-    graph.append(list(input()))
-dx = [-1, 0, 1]
-visit = [[False]*c for _ in range(r)]
-ans = 0
+g = int(input())
+p = int(input())
+plane = []
+for _ in range(p):
+    plane.append(int(input()))
 
 
-def solve(i, j):
-    if j == c - 1:
-        return True
-
-    for d in dx:
-        if 0 <= i + d < r and graph[i+d][j+1] == '.' and not visit[i+d][j+1]:
-            visit[i+d][j+1] = True
-            if solve(i+d, j+1):
-                return True
-    return False
+def parent_find(x):
+    if x == parent[x]:
+        return x
+    p = parent_find(parent[x])
+    parent[x] = p
+    return parent[x]
 
 
-for i in range(r):
-    if graph[i][0] == '.':
-        if solve(i, 0):
-            ans += 1
+def union(x, y):
+    x = parent_find(x)
+    y = parent_find(y)
+    parent[x] = y
 
-print(ans)
+
+parent = {i:i for i in range(g+1)}
+cnt = 0
+for i in plane:
+    x = parent_find(i)
+    if x == 0:
+        break
+    union(x, x-1)
+    cnt += 1
+
+print(cnt)
+
