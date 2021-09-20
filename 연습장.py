f4af9d62bac03ba2ee44e58f35b3c6ed9fa8de60
yllausac <dadanana0805@gmail.com>
2021-09-20 19:13:27 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,35 +1,24 @@
 import sys
+import heapq
 
 input = sys.stdin.readline
-g = int(input())
-p = int(input())
-plane = []
-for _ in range(p):
-    plane.append(int(input()))
+n = int(input())
+class_ = []
+for _ in range(n):
+    s, t = map(int, input().split())
+    class_.append([s, t])
+class_.sort()
 
+room = []
+heapq.heappush(room, class_[0][1])
 
-def parent_find(x):
-    if x == parent[x]:
-        return x
-    p = parent_find(parent[x])
-    parent[x] = p
-    return parent[x]
+for i in range(1, n):
+    if class_[i][0] < room[0]:
+        heapq.heappush(room, class_[i][1])
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, class_[i][1])
 
+print(len(room))
 
-def union(x, y):
-    x = parent_find(x)
-    y = parent_find(y)
-    parent[x] = y
-
-
-parent = {i:i for i in range(g+1)}
-cnt = 0
-for i in plane:
-    x = parent_find(i)
-    if x == 0:
-        break
-    union(x, x-1)
-    cnt += 1
-
-print(cnt)
 
