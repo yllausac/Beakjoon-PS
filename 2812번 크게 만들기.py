cef80fb9e139ba69d6cfad6a1d177a7463c4ea3e
yllausac <dadanana0805@gmail.com>
2021-09-23 22:44:51 +0900

    BOJ

--- 2812번 크게 만들기.py
+++ 2812번 크게 만들기.py
@@ -2,12 +2,12 @@ import sys
 
 input = sys.stdin.readline
 n, k = map(int, input().split())
-number = list(map(int, input().rstrip()))
+number = list(map(int, input().strip()))
 result = []
 
 for i in range(n):
     while k > 0 and result:
-        if result[len(result)-1] < number[i]:
+        if result[-1] < number[i]:
             result.pop()
             k -= 1
         else:
