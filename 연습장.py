d60c7cbee53db1f901a88b3e3bf84c16623b656e
yllausac <dadanana0805@gmail.com>
2021-09-25 11:15:11 +0900

    BOJ

--- 연습장.py
+++ 연습장.py
@@ -1,21 +1,50 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
-n, k = map(int, input().split())
-number = list(map(int, input().strip()))
-result = []
-delnum = k
+n, m = map(int, input().split())
+before = [list(input().strip().split()) for _ in range(n)]
+after = [list(input().strip().split()) for _ in range(n)]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
-for i in range(n):
-    while delnum > 0 and result:
-        if result[-1] < number[i]:
-            result.pop()
-            delnum -= 1
-        else:
-            break
-    result.append(number[i])
 
-for i in range(n-k):
-    print(result[i], end='')
+def solv():
+    for x in range(n):
+        for y in range(m):
+            if before[x][y] != after[x][y]:
+                bfs(x, y)
+                return check()
+    return 'YES'
 
+
+def bfs(x, y):
+    global before
+    visited = [[False] * m for _ in range(n)]
+    queue = deque([(x, y)])
+
+    visited[x][y] = True
+    target = before[x][y]
+    num = after[x][y]
+    while queue:
+        cx, cy = queue.pop()
+        before[cx][cy] = num
+
+        for k in range(4):
+            nx = cx + dx[k]
+            ny = cy + dy[k]
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == False:
+                if before[nx][ny] == target:
+                    visited[nx][ny] = True
+                    queue.appendleft((nx, ny))
+
+
+def check():
+    for x in range(n):
+        for y in range(m):
+            if before[x][y] != after[x][y]:
+                return 'NO'
+    return 'YES'
+
+
+print(solv())
 
